97a57fa41674f98fc610c0e7b0a929fbba689377
rms <rms@rms80.com>
2017-01-13 16:48:44 -0500

    add stl ascii/binary writing, a few utility things

---
+++ io/STLWriter.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace g3
+{
+
+    public class STLWriter : IMeshWriter
+    {
+        // entire data block for stl triangle, that we can directly convert to byte[]
+        [StructLayout(LayoutKind.Sequential, Pack=1)]
+        struct stl_triangle
+        {
+            public float nx, ny, nz;
+            public float ax, ay, az;
+            public float bx, by, bz;
+            public float cx, cy, cz;
+            public short attrib;
+        }
+
+
+        public IOWriteResult Write(BinaryWriter writer, List<WriteMesh> vMeshes, WriteOptions options)
+        {
+            string header = "g3sharp_stl ";
+            byte[] header_bytes = ASCIIEncoding.ASCII.GetBytes(header);
+            byte[] stl_header = new byte[80];
+            Array.Clear(stl_header, 0, stl_header.Length);
+            Array.Copy(header_bytes, stl_header, header_bytes.Length);
+
+            writer.Write(stl_header);
+
+            int total_tris = 0;
+            foreach (WriteMesh mesh in vMeshes)
+                total_tris += mesh.Mesh.TriangleCount;
+            writer.Write(total_tris);
+
+            for (int mi = 0; mi < vMeshes.Count; ++mi) {
+                IMesh mesh = vMeshes[mi].Mesh;
+
+
+                Func<int, stl_triangle> producerF = (ti) => {
+                    stl_triangle tri = new stl_triangle();
+                    Index3i t = mesh.GetTriangle(ti);
+                    Vector3d a = mesh.GetVertex(t.a), b = mesh.GetVertex(t.b), c = mesh.GetVertex(t.c);
+                    Vector3d n = MathUtil.Normal(a, b, c);
+
+                    tri.nx = (float)n.x; tri.ny = (float)n.y; tri.nz = (float)n.z;
+                    tri.ax = (float)a.x; tri.ay = (float)a.y; tri.az = (float)a.z;
+                    tri.bx = (float)b.x; tri.by = (float)b.y; tri.bz = (float)b.z;
+                    tri.cx = (float)c.x; tri.cy = (float)c.y; tri.cz = (float)c.z;
+                    tri.attrib = 0;
+                    return tri;
+                };
+                Action<stl_triangle> consumerF = (tri) => {
+                    byte[] tri_bytes = Util.StructureToByteArray(tri);
+                    writer.Write(tri_bytes);
+                };
+
+                ParallelStream<int, stl_triangle> stream = new ParallelStream<int, stl_triangle>();
+                stream.ProducerF = producerF;
+                stream.ConsumerF = consumerF;
+
+                // parallel version is slower =\
+                //stream.Run_Thread(mesh.TriangleIndices());
+                stream.Run(mesh.TriangleIndices());
+            }
+
+            return new IOWriteResult(IOCode.Ok, "");
+        }
+
+
+
+
+
+
+        public IOWriteResult Write(TextWriter writer, List<WriteMesh> vMeshes, WriteOptions options)
+        {
+            if (options.bCombineMeshes == true)
+                writer.WriteLine("solid \"mesh\"");
+
+            string three_floats = Util.MakeVec3FormatString(0, 1, 2, options.RealPrecisionDigits);
+
+            for (int mi = 0; mi < vMeshes.Count; ++mi) {
+
+                IMesh mesh = vMeshes[mi].Mesh;
+
+                string solid_name = string.Format("mesh_{0}", mi);
+                if (options.bCombineMeshes == false) {
+                    if (vMeshes[mi].Name.Length > 0)
+                        solid_name = vMeshes[mi].Name;
+                    writer.WriteLine("solid \"{0}\"", solid_name);
+                }
+
+                foreach ( int ti in mesh.TriangleIndices() ) {
+                    Index3i t = mesh.GetTriangle(ti);
+                    Vector3d a = mesh.GetVertex(t.a), b = mesh.GetVertex(t.b), c = mesh.GetVertex(t.c);
+                    Vector3d n = MathUtil.Normal(a, b, c);
+                    writer.WriteLine("facet normal " + three_floats, n.x, n.y, n.z);
+                    writer.WriteLine("outer loop" + writer.NewLine + "vertex " + three_floats, a.x, a.y, a.z);
+                    writer.WriteLine("vertex " + three_floats, b.x, b.y, b.z);
+                    writer.WriteLine("vertex " + three_floats, c.x, c.y, c.z);
+                    writer.WriteLine("endloop" + writer.NewLine + "endfacet");
+                }
+
+                if (options.bCombineMeshes == false)
+                    writer.WriteLine("endsolid \"{0}\"", solid_name);
+            }
+
+            if (options.bCombineMeshes == true)
+                writer.WriteLine("endsolid \"mesh\"");
+
+            return new IOWriteResult(IOCode.Ok, "");
+        }
+
+
+    }
+}
--- io/StandardMeshWriter.cs
+++ io/StandardMeshWriter.cs
@@ -9,42 +9,73 @@ namespace g3
     public class StandardMeshWriter
     {
 
-        static public IOWriteResult WriteFile(string sFilename, List<IMesh> vMeshes, WriteOptions options)
+        static public IOWriteResult WriteFile(string sFilename, List<WriteMesh> vMeshes, WriteOptions options)
         {
             StandardMeshWriter writer = new StandardMeshWriter();
             return writer.Write(sFilename, vMeshes, options);
         }
 
 
-        public IOWriteResult Write(string sFilename, List<IMesh> vMeshes, WriteOptions options)
+        public IOWriteResult Write(string sFilename, List<WriteMesh> vMeshes, WriteOptions options)
         {
-            string sExtension = Path.GetExtension(sFilename).ToUpper();
+            Func<string, List<WriteMesh>, WriteOptions, IOWriteResult> writeFunc = null;
 
-            if (sExtension == ".OBJ") {
-                try {
-                    return Write_OBJ(sFilename, vMeshes, options);
-                } catch (Exception) {
-                    return new IOWriteResult(WriteResult.WriterError, "Unknown error");
-                }
 
-            } else
-                return new IOWriteResult(WriteResult.UnknownFormatError, "cannot write file format " + sExtension);
+            string sExtension = Path.GetExtension(sFilename);
+            if (sExtension.Equals(".obj", StringComparison.OrdinalIgnoreCase))
+                writeFunc = Write_OBJ;
+            else if (sExtension.Equals(".stl", StringComparison.OrdinalIgnoreCase))
+                writeFunc = Write_STL;
 
+            if (writeFunc == null)
+                return new IOWriteResult(IOCode.UnknownFormatError, "format " + sExtension + " is not supported");
+
+            try {
+                return writeFunc(sFilename, vMeshes, options);
+            } catch (Exception) {
+                return new IOWriteResult(IOCode.WriterError, "Unknown error");
+            }
         }
 
 
 
 
-        IOWriteResult Write_OBJ(string sFilename, List<IMesh> vMeshes, WriteOptions options)
+        IOWriteResult Write_OBJ(string sFilename, List<WriteMesh> vMeshes, WriteOptions options)
         {
             StreamWriter w = new StreamWriter(sFilename);
             if (w.BaseStream == null)
-                return new IOWriteResult(WriteResult.FileAccessError, "Could not open file " + sFilename + " for writing");
+                return new IOWriteResult(IOCode.FileAccessError, "Could not open file " + sFilename + " for writing");
 
             OBJWriter writer = new OBJWriter();
             var result = writer.Write(w, vMeshes, options);
             w.Close();
             return result;
+        }
+
+
+
+        IOWriteResult Write_STL(string sFilename, List<WriteMesh> vMeshes, WriteOptions options)
+        {
+            if (options.bWriteBinary) {
+                FileStream file_stream = File.Open(sFilename, FileMode.Truncate);
+                BinaryWriter w = new BinaryWriter(file_stream);
+                if (w.BaseStream == null)
+                    return new IOWriteResult(IOCode.FileAccessError, "Could not open file " + sFilename + " for writing");
+                STLWriter writer = new STLWriter();
+                var result = writer.Write(w, vMeshes, options);
+                w.Close();
+                file_stream.Close();
+                return result;
+
+            } else {
+                StreamWriter w = new StreamWriter(sFilename);
+                if (w.BaseStream == null)
+                    return new IOWriteResult(IOCode.FileAccessError, "Could not open file " + sFilename + " for writing");
+                STLWriter writer = new STLWriter();
+                var result = writer.Write(w, vMeshes, options);
+                w.Close();
+                return result;
+            }
         }
 
 
