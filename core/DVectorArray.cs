74f2dc59c5e19c907841b90971272964483fbf10
rms <rms@rms80.com>
2017-01-16 12:35:18 -0500

    temp checkin

---
+++ core/DVectorArray.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace g3
+{
+    //
+    // This class is just a wrapper around a static array that provides convenient 3-element set/get access
+    // Useful for things like treating a float array as a list of vectors
+    //
+    public class DVectorArray3<T> : IEnumerable<T>
+    {
+        public DVector<T> vector;
+
+        public DVectorArray3(int nCount = 0) {
+            vector = new DVector<T>();
+            if (nCount > 0)
+                vector.resize(nCount*3);
+
+        }
+
+        public DVectorArray3(T[] data) {
+            vector = new DVector<T>(data);
+        }
+
+        public int Count {
+            get { return vector.Length/3; }
+        }
+
+        // we should just be passing back DVector enumerator, but it doesn't have one??
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < vector.Length; ++i)
+                yield return vector[i];
+        }
+
+        public void Resize(int count) {
+            vector.resize(3 * count);
+        }
+
+        public void Set(int i, T a, T b, T c) {
+            vector.insert(a, 3 * i);
+            vector.insert(b, 3 * i+1);
+            vector.insert(c, 3 * i+2);
+        }
+
+        public void Append(int i, T a, T b, T c) {
+            vector.push_back(a);
+            vector.push_back(b);
+            vector.push_back(c);
+        }
+
+        //public void Set(int iStart, int iCount, DVectorArray3<T> source) {
+        //	Array.Copy(source.vector, 0, vector, 3*iStart, 3*iCount);
+        //}
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();// vector.GetEnumerator();
+        }
+    }
+
+
+    public class DVectorArray3d : DVectorArray3<double>
+    {
+        const double invalid_value = -99999999.0;
+
+        public DVectorArray3d(int nCount = 0) : base(nCount)
+        {
+        }
+        public DVectorArray3d(double[] data) : base(data) { }
+        public Vector3d this[int i] {
+            get { return new Vector3d(vector[3 * i], vector[3 * i + 1], vector[3 * i + 2]); }
+            set { Set(i, value[0], value[1], value[2]); }
+        }
+
+        public IEnumerable<Vector3d> AsVector3d() {
+            for (int i = 0; i < Count; ++i)
+                yield return this[i];
+        }
+    };
+
+
+    public class DVectorArray3f : DVectorArray3<float>
+    {
+        public DVectorArray3f(int nCount = 0) : base(nCount) { }
+        public DVectorArray3f(float[] data) : base(data) { }
+        public Vector3f this[int i] {
+            get { return new Vector3f(vector[3 * i], vector[3 * i + 1], vector[3 * i + 2]); }
+            set { Set(i, value[0], value[1], value[2]); }
+        }
+
+        public IEnumerable<Vector3f> AsVector3f()
+        {
+            for (int i = 0; i < Count; ++i)
+                yield return this[i];
+        }
+    };
+
+
+    public class DVectorArray3i : DVectorArray3<int>
+    {
+        public DVectorArray3i(int nCount = 0) : base(nCount) { }
+        public DVectorArray3i(int[] data) : base(data) { }
+        public Vector3i this[int i] {
+            get { return new Vector3i(vector[3 * i], vector[3 * i + 1], vector[3 * i + 2]); }
+            set { Set(i, value[0], value[1], value[2]); }
+        }
+        // [RMS] for CW/CCW codes
+        public void Set(int i, int a, int b, int c, bool bCycle = false) {
+            vector[3 * i] = a;
+            if (bCycle) {
+                vector[3 * i + 1] = c;
+                vector[3 * i + 2] = b;
+            } else {
+                vector[3 * i + 1] = b;
+                vector[3 * i + 2] = c;
+            }
+        }
+
+        public IEnumerable<Vector3i> AsVector3i()
+        {
+            for (int i = 0; i < Count; ++i)
+                yield return this[i];
+        }
+    };
+
+
+
+    public class DIndexArray3i : DVectorArray3<int>
+    {
+        public DIndexArray3i(int nCount = 0) : base(nCount) { }
+        public DIndexArray3i(int[] data) : base(data) { }
+        public Index3i this[int i] {
+            get { return new Index3i(vector[3 * i], vector[3 * i + 1], vector[3 * i + 2]); }
+            set { Set(i, value[0], value[1], value[2]); }
+        }
+        // [RMS] for CW/CCW codes
+        public void Set(int i, int a, int b, int c, bool bCycle = false) {
+            vector[3 * i] = a;
+            if (bCycle) {
+                vector[3 * i + 1] = c;
+                vector[3 * i + 2] = b;
+            } else {
+                vector[3 * i + 1] = b;
+                vector[3 * i + 2] = c;
+            }
+        }
+
+        public IEnumerable<Index3i> AsIndex3i()
+        {
+            for (int i = 0; i < Count; ++i)
+                yield return new Index3i(vector[3 * i], vector[3 * i + 1], vector[3 * i + 2]);
+        }
+    };
+
+
+
+
+
+    //
+    // Same as DVectorArray3, but for 2D vectors/etc
+    //
+    public class DVectorArray2<T> : IEnumerable<T>
+    {
+        public DVector<T> vector;
+
+        public DVectorArray2(int nCount = 0)
+        {
+            vector = new DVector<T>();
+            vector.resize(nCount * 2);
+        }
+
+        public DVectorArray2(T[] data)
+        {
+            vector = new DVector<T>(data);
+        }
+
+        public int Count
+        {
+            get { return vector.Length / 2; }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < vector.Length; ++i)
+                yield return vector[i];
+        }
+
+        public void Resize(int count) {
+            vector.resize(2* count);
+        }
+
+        public void Set(int i, T a, T b) {
+            vector.insert(a, 2 * i);
+            vector.insert(b, 2 * i+1);
+        }
+
+        public void Append(int i, T a, T b) {
+            vector.push_back(a);
+            vector.push_back(b);
+        }
+
+        //public void Set(int iStart, int iCount, DVectorArray2<T> source) {
+        //	Array.Copy(source.vector, 0, vector, 2*iStart, 2*iCount);
+        //}
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            //return vector.GetEnumerator();
+            return this.GetEnumerator();
+        }
+    }
+    public class DVectorArray2d : DVectorArray2<double>
+    {
+        public DVectorArray2d(int nCount = 0) : base(nCount) { }
+        public DVectorArray2d(double[] data) : base(data) { }
+        public Vector2d this[int i] {
+            get { return new Vector2d(vector[2 * i], vector[2 * i + 1]); }
+            set { Set(i, value[0], value[1]); }
+        }
+
+        public IEnumerable<Vector2d> AsVector2d() {
+            for (int i = 0; i < Count; ++i)
+                yield return this[i];
+        }
+    };
+    public class DVectorArray2f : DVectorArray2<float>
+    {
+        public DVectorArray2f(int nCount = 0) : base(nCount) { }
+        public DVectorArray2f(float[] data) : base(data) { }
+        public Vector2f this[int i] {
+            get { return new Vector2f(vector[2 * i], vector[2 * i + 1]); }
+            set { Set(i, value[0], value[1]); }
+        }
+
+        public IEnumerable<Vector2d> AsVector2f() {
+            for (int i = 0; i < Count; ++i)
+                yield return this[i];
+        }
+    };
+
+
+
+    public class DIndexArray2i : DVectorArray2<int>
+    {
+        public DIndexArray2i(int nCount = 0) : base(nCount) { }
+        public DIndexArray2i(int[] data) : base(data) { }
+        public Index2i this[int i] {
+            get { return new Index2i(vector[2 * i], vector[2 * i + 1]); }
+            set { Set(i, value[0], value[1]); }
+        }
+
+        public IEnumerable<Index2i> AsIndex2i() {
+            for (int i = 0; i < Count; ++i)
+                yield return new Index2i(vector[2 * i], vector[2 * i + 1]);
+        }
+    };
+
+
+}
---
+++ io/STLReader.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace g3
+{
+    class STLReader : IMeshReader
+    {
+
+
+        // connect to this to get warning messages
+		public event ErrorEventHandler warningEvent;
+
+        int nWarningLevel = 0;      // 0 == no diagnostics, 1 == basic, 2 == crazy
+        Dictionary<string, int> warningCount = new Dictionary<string, int>();
+
+
+
+
+        class TriangleList
+        {
+            public string Name;
+            public DVectorArray3d Vertices = new DVectorArray3d();
+        }
+
+
+        List<TriangleList> Objects;
+
+        void append_vertex(float x, float y, float z)
+        {
+
+        }
+
+
+
+        public IOReadResult Read(BinaryReader reader, ReadOptions options, IMeshBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IOReadResult Read(TextReader reader, ReadOptions options, IMeshBuilder builder)
+        {
+
+            //solid "stl_ascii"
+            //  facet normal 0.722390830517 -0.572606861591 0.387650430202
+            //    outer loop
+            //      vertex 0.00659640412778 4.19127035141 -0.244179025292
+            //      vertex -0.0458636470139 4.09951019287 -0.281960010529
+            //      vertex 0.0286951716989 4.14693021774 -0.350856184959
+            //    endloop
+            //  endfacet
+
+            bool in_solid = false;
+            bool in_facet = false;
+            bool in_loop = false;
+            int vertices_in_loop = 0;
+
+            Objects = new List<TriangleList>();
+
+            int nLines = 0;
+            while (reader.Peek() >= 0) {
+
+                string line = reader.ReadLine();
+                nLines++;
+                string[] tokens = line.Split( (char[])null , StringSplitOptions.RemoveEmptyEntries);
+                if (tokens.Length == 0)
+                    continue;
+
+                if (tokens[0].Equals("vertex", StringComparison.OrdinalIgnoreCase)) {
+
+                } else if (tokens[0].Equals("outer", StringComparison.OrdinalIgnoreCase)) {
+                    in_loop = true;
+                    vertices_in_loop = 0;
+
+                } else if (tokens[0].Equals("endloop", StringComparison.OrdinalIgnoreCase)) {
+                    in_loop = false;
+                        
+
+                } else if (tokens[0].Equals("facet", StringComparison.OrdinalIgnoreCase)) {
+                    if ( in_solid == false ) {      // handle bad STL
+                        Objects.Add(new TriangleList() { Name = "unknown_solid" });
+                        in_solid = true;
+                    }
+                    in_facet = true;
+                    // ignore facet normal
+
+                } else if (tokens[0].Equals("endfacet", StringComparison.OrdinalIgnoreCase)) {
+                    in_facet = false;
+
+
+                } else if (tokens[0].Equals("solid", StringComparison.OrdinalIgnoreCase)) {
+                    TriangleList newObj = new TriangleList();
+                    if (tokens.Length == 2)
+                        newObj.Name = tokens[1];
+                    else
+                        newObj.Name = "object_" + Objects.Count;
+                    Objects.Add(newObj);
+                    in_solid = true;
+
+                } else if (tokens[0].Equals("endsolid", StringComparison.OrdinalIgnoreCase)) {
+                    // do nothing, done object
+                    in_solid = false;
+                }
+
+            }
+        }
+
+
+
+
+        private void emit_warning(string sMessage)
+        {
+            string sPrefix = sMessage.Substring(0, 15);
+            int nCount = warningCount.ContainsKey(sPrefix) ? warningCount[sPrefix] : 0;
+            nCount++; warningCount[sPrefix] = nCount;
+            if (nCount > 10)
+                return;
+            else if (nCount == 10)
+                sMessage += " (additional message surpressed)";
+
+            var e = warningEvent;
+            if ( e != null ) 
+                e(this, new ErrorEventArgs(new Exception(sMessage)));
+        }
+
+    }
+}
