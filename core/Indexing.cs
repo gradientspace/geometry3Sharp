c20439021180fed7b9f6c014484b4660e101352e
rms <rms@rms80.com>
2017-02-03 14:47:58 -0500

    DSubmesh3 extracts a submesh from a mesh, holds relevant maps/etc

---
+++ core/Indexing.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+
+namespace g3
+{
+
+    // This class provides a similar interface to BitArray, but can optionally
+    // use a HashSet (or perhaps some other DS) if the fraction of the index space 
+    // required is small
+    public class IndexFlagSet
+    {
+        BitArray bits;
+        HashSet<int> hash;
+
+
+        public IndexFlagSet(bool bForceSparse, int MaxIndex = -1)
+        {
+            if (bForceSparse) {
+                hash = new HashSet<int>();
+            } else {
+                bits = new BitArray(MaxIndex);
+            }
+        }
+
+        public IndexFlagSet(int MaxIndex, int SubsetCountEst)
+        {
+            bool bSmall = MaxIndex < 128000;        // 16k in bits is a pretty small buffer?
+            float fPercent = (float)SubsetCountEst / (float)MaxIndex;
+            float fPercentThresh = 0.05f;           
+
+            if (bSmall || fPercent > fPercentThresh) { 
+                bits = new BitArray(MaxIndex);
+            } else 
+                hash = new HashSet<int>();
+        }
+
+        public bool this[int key]
+        {
+            get {
+                return (bits != null) ? bits[key] : hash.Contains(key);
+            }
+            set {
+                if (bits != null) {
+                    bits[key] = value;
+                } else {
+                    if (value == true)
+                        hash.Add(key);
+                    else if (value == false && hash.Contains(key))
+                        hash.Remove(key);
+                }
+            }
+        }
+
+    }
+
+
+
+
+
+    // dense or sparse index map
+    public class IndexMap
+    {
+        // this is returned if sparse map doesn't contain value
+        public readonly int InvalidIndex = int.MinValue;
+
+
+        int[] dense_map;
+        Dictionary<int, int> sparse_map;
+        int MaxIndex;
+
+        public IndexMap(bool bForceSparse, int MaxIndex = -1)
+        {
+            if (bForceSparse) {
+                sparse_map = new Dictionary<int, int>();
+            } else {
+                dense_map = new int[MaxIndex];
+            }
+            this.MaxIndex = MaxIndex;
+        }
+
+        public IndexMap(int MaxIndex, int SubsetCountEst)
+        {
+            bool bSmall = MaxIndex < 32000;        // if buffer is less than 128k, just use dense map
+            float fPercent = (float)SubsetCountEst / (float)MaxIndex;
+            float fPercentThresh = 0.1f;
+
+            if (bSmall || fPercent > fPercentThresh) {
+                dense_map = new int[MaxIndex];
+            } else {
+                sparse_map = new Dictionary<int, int>();
+            }
+            this.MaxIndex = MaxIndex;
+        }
+
+
+        // no effect on sparse map
+        public void SetToInvalid()
+        {
+            if ( dense_map != null ) {
+                for (int i = 0; i < dense_map.Length; ++i)
+                    dense_map.SetValue(i, InvalidIndex);
+            }
+        }
+
+
+        // dense variant: returns true unless you have set index to InvalidIndex (eg via SetToInvalid)
+        // sparse variant: returns true if index is in map
+        // either: returns false if index is out-of-bounds
+        public bool Contains(int index)
+        {
+            if (MaxIndex > 0 && index >= MaxIndex)
+                return false;
+            if (dense_map != null)
+                return dense_map[index] != InvalidIndex;
+            else
+                return sparse_map.ContainsKey(index);
+        }
+
+
+
+        public int this[int index]
+        {
+            get {
+                if (dense_map != null)
+                    return dense_map[index];
+                else {
+                    int to;
+                    if (sparse_map.TryGetValue(index, out to))
+                        return to;
+                    return InvalidIndex;
+                }
+            }
+            set {
+                if (dense_map != null) {
+                    dense_map[index] = value;
+                } else {
+                    sparse_map[index] = value;
+                }
+            }
+        }
+
+    }
+
+
+
+}
---
+++ mesh/DSubmesh3.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace g3
+{
+
+
+
+
+    public class DSubmesh3
+    {
+        public DMesh3 BaseMesh;
+        public DMesh3 SubMesh;
+        public MeshComponents WantComponents = MeshComponents.All;
+
+        public IndexFlagSet BaseSubmeshV;       // we compute this anyway, might as well hang onto it
+        public IndexMap BaseToSubV;
+        public DVector<int> SubToBaseV;
+
+
+        // boundary info
+        public IndexFlagSet BaseBorderV;
+
+
+        public DSubmesh3(DMesh3 mesh, int[] subTriangles)
+        {
+            BaseMesh = mesh;
+            compute(subTriangles, subTriangles.Length);
+        }
+
+
+        public void ComputeBoundaryInfo(int[] subTriangles) {
+            ComputeBoundaryInfo(subTriangles, subTriangles.Length);
+        }
+        public void ComputeBoundaryInfo(IEnumerable<int> triangles, int tri_count)
+        {
+            IndexFlagSet sub_tris = new IndexFlagSet(BaseMesh.MaxTriangleID, tri_count);
+            foreach (int ti in triangles)
+                sub_tris[ti] = true;
+
+            BaseBorderV = new IndexFlagSet(true, BaseMesh.MaxVertexID);
+
+            // this processes each edge internal edge twice...but it is cheap
+            foreach (int ti in triangles) {
+                Index3i tedges = BaseMesh.GetTriEdges(ti);
+                for ( int j = 0; j < 3; ++j ) {
+                    int eid = tedges[j];
+                    Index2i tris = BaseMesh.GetEdgeT(eid);
+                    if (tris.b == DMesh3.InvalidID || sub_tris[tris.a] != sub_tris[tris.b]) { 
+                        Index2i ve = BaseMesh.GetEdgeV(eid);
+                        BaseBorderV[ve.a] = true;
+                        BaseBorderV[ve.b] = true;
+                    } 
+                }
+            }
+            
+        }
+
+
+
+
+        void compute(IEnumerable<int> triangles, int tri_count )
+        {
+            int est_verts = tri_count / 2;
+
+            SubMesh = new DMesh3( BaseMesh.Components & WantComponents );
+
+            BaseSubmeshV = new IndexFlagSet(BaseMesh.MaxVertexID, est_verts);
+            BaseToSubV = new IndexMap(BaseMesh.MaxVertexID, est_verts);
+            SubToBaseV = new DVector<int>();
+
+            foreach ( int ti in triangles ) {
+                Index3i base_t = BaseMesh.GetTriangle(ti);
+                Index3i new_t = Index3i.Zero;
+                int gid = BaseMesh.GetTriangleGroup(ti);
+
+                for ( int j = 0; j < 3; ++j ) {
+                    int base_v = base_t[j];
+                    int sub_v = -1;
+                    if (BaseSubmeshV[base_v] == false) {
+                        sub_v = SubMesh.AppendVertex(BaseMesh, base_v);
+                        BaseSubmeshV[base_v] = true;
+                        BaseToSubV[base_v] = sub_v;
+                        SubToBaseV.insert(base_v, sub_v);
+                    } else
+                        sub_v = BaseToSubV[base_v];
+                    new_t[j] = sub_v;
+                }
+
+                SubMesh.AppendTriangle(new_t, gid);
+            }
+
+
+
+
+        }
+
+
+
+
+    }
+}
