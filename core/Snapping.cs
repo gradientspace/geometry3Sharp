f5bc0e78f4f96fb5cda5843ca02f0a7da568fc82
rms <rms@rms80.com>
2017-02-03 10:14:45 -0500

    snapping

---
+++ core/Snapping.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace g3
+{
+    public class Snapping
+    {
+
+        public static double SnapToIncrement(double fValue, double fIncrement)
+        {
+            double sign = Math.Sign(fValue);
+            fValue = Math.Abs(fValue);
+            int nInc = (int)(fValue / fIncrement);
+            double fRem = Math.IEEERemainder(fValue, fIncrement);
+            if (fRem > fIncrement / 2)
+                ++nInc;
+            return sign * (double)nInc * fIncrement;
+        }
+
+
+    }
+}
