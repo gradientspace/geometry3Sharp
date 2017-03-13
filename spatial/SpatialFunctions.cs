42a71793e3e519b8280e5388e352d4b5f51e7e05
rms <rms@rms80.com>
2017-03-13 15:51:02 -0400

    some utility things

---
+++ spatial/SpatialFunctions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace g3
+{
+
+    // collection of utility classes
+    public static class SpatialFunctions
+    {
+
+        // various offset-surface functions, in class so the compute functions 
+        // can be passed to other functions
+        public class NormalOffset
+        {
+            public DMesh3 Mesh;
+            public ISpatial Spatial;
+
+            public double Distance = 0.01;
+            public bool UseFaceNormal = true;
+
+            public Vector3d FindNearestAndOffset(Vector3d pos)
+            {
+                int tNearestID = Spatial.FindNearestTriangle(pos);
+                DistPoint3Triangle3 q = MeshQueries.TriangleDistance(Mesh, tNearestID, pos);
+                Vector3d vHitNormal = 
+                    (UseFaceNormal == false && Mesh.HasVertexNormals) ?
+                        Mesh.GetTriBaryNormal(tNearestID, q.TriangleBaryCoords.x, q.TriangleBaryCoords.y, q.TriangleBaryCoords.z) 
+                        : Mesh.GetTriNormal(tNearestID);
+                return q.TriangleClosest + Distance * vHitNormal;
+            }
+
+        }
+
+
+    }
+
+
+
+}
