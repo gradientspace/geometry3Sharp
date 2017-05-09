4a34ebb00ccefc0a5cc880e38d7ea2714ebc712c
rms <rms@rms80.com>
2017-05-09 17:10:56 -0400

    support for no-units, string->units convert

--- core/Units.cs
+++ core/Units.cs
@@ -15,6 +15,8 @@ namespace g3
 
         public enum Linear
         {
+            UnknownUnits = 0,
+
             // numbers are 20 + power-of-ten  
             Nanometers = 11,        // 10^-9
             Microns = 14,           // 10^-6
@@ -35,13 +37,13 @@ namespace g3
 
         public static bool IsMetric(Linear t)
         {
-            return (int)t < 50;     // based on enum above
+            return t > 0 && (int)t < 50;     // based on enum above
         }
 
 
         public static double GetMetricPower(Linear t)
         {
-            if ((int)t < 50)
+            if (t > 0 && (int)t < 50)
                 return (double)t - 20.0;        // based on enum values above
             throw new Exception("Units.GetMetricPower: input unit is not metric!");
         }
@@ -49,7 +51,7 @@ namespace g3
 
         public static double ToMeters(Linear t)
         {
-            if ( (int)t < 50 ) {
+            if ( t > 0 && (int)t < 50 ) {
                 double d = GetMetricPower(t);
                 return Math.Pow(10, d);
             }
@@ -69,7 +71,7 @@ namespace g3
 
         public static double MetersTo(Linear t)
         {
-            if ( (int)t < 50 ) {
+            if ( t > 0 && (int)t < 50 ) {
                 double d = GetMetricPower(t);
                 return Math.Pow(10, -d);
             }
@@ -103,9 +105,41 @@ namespace g3
 
 
 
+        public static Linear ParseLinear(string units)
+        {
+            // [TODO] could switch on first character? would be more efficient
+            if (units == "nm")
+                return Linear.Nanometers;
+            else if (units == "um")
+                return Linear.Microns;
+            else if (units == "mm")
+                return Linear.Millimeters;
+            else if (units == "cm")
+                return Linear.Centimeters;
+            else if (units == "m")
+                return Linear.Meters;
+            else if (units == "km")
+                return Linear.Kilometers;
+
+            else if (units == "in")
+                return Linear.Inches;
+            else if (units == "ft")
+                return Linear.Feet;
+            else if (units == "yd")
+                return Linear.Yards;
+            else if (units == "mi")
+                return Linear.Miles;
+
+            return Linear.UnknownUnits;
+        }
+
+
+
         public static string GetShortString(Linear unit)
         {
             switch (unit) {
+                case Linear.UnknownUnits:
+                    return "??";
                 case Linear.Nanometers:
                     return "nm";
                 case Linear.Microns:
