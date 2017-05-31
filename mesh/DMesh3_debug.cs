5da8a5b9551f688c07cd413988b4c561e35f3c52
rms <rms@rms80.com>
2017-05-31 16:28:27 -0400

    made CheckValidity failure behavior configurable

--- mesh/DMesh3_debug.cs
+++ mesh/DMesh3_debug.cs
@@ -40,34 +40,50 @@ namespace g3
 		}
 
 
-        // debug
 
-        public void DMESH_CHECK_OR_FAIL(bool b) { Util.gDevAssert(b); }
+        public enum FailMode { DebugAssert, gDevAssert, Throw, ReturnOnly }
 
+        /// <summary>
         // This function checks that the mesh is well-formed, ie all internal data
         // structures are consistent
-        public bool CheckValidity(bool bAllowNonManifoldVertices = false) {
+        /// </summary>
+        public bool CheckValidity(bool bAllowNonManifoldVertices = false, FailMode eFailMode = FailMode.Throw ) {
 
 			int[] triToVtxRefs = new int[this.MaxVertexID];
 
+            bool is_ok = true;
+            Action<bool> CheckOrFailF = (b) => {
+                is_ok = is_ok && b;
+            };
+            if ( eFailMode == FailMode.DebugAssert ) {
+                CheckOrFailF = (b) => { Debug.Assert(b);
+                                        is_ok = is_ok && b; };
+            } else if ( eFailMode == FailMode.gDevAssert ) {
+                CheckOrFailF = (b) => { Util.gDevAssert(b);
+                                        is_ok = is_ok && b; };
+            } else if ( eFailMode == FailMode.Throw ) {
+                CheckOrFailF = (b) => { if (b == false)
+                                            throw new Exception("DMesh3.CheckValidity: check failed"); };
+            }
+
 			if ( normals != null )
-				DMESH_CHECK_OR_FAIL(normals.size == vertices.size);
+				CheckOrFailF(normals.size == vertices.size);
 			if ( colors != null )
-				DMESH_CHECK_OR_FAIL(colors.size == vertices.size);
+				CheckOrFailF(colors.size == vertices.size);
 			if ( uv != null )
-				DMESH_CHECK_OR_FAIL(uv.size/2 == vertices.size/3);
+				CheckOrFailF(uv.size/2 == vertices.size/3);
 			if ( triangle_groups != null )
-				DMESH_CHECK_OR_FAIL(triangle_groups.size == triangles.size/3);
+				CheckOrFailF(triangle_groups.size == triangles.size/3);
 
             foreach (int tID in TriangleIndices() ) { 
 
-                DMESH_CHECK_OR_FAIL(IsTriangle(tID));
-                DMESH_CHECK_OR_FAIL(triangles_refcount.refCount(tID) == 1);
+                CheckOrFailF(IsTriangle(tID));
+                CheckOrFailF(triangles_refcount.refCount(tID) == 1);
 
                 // vertices must exist
                 Index3i tv = GetTriangle(tID);
                 for (int j = 0; j < 3; ++j) {
-                    DMESH_CHECK_OR_FAIL(IsVertex(tv[j]));
+                    CheckOrFailF(IsVertex(tv[j]));
                     triToVtxRefs[tv[j]] += 1;
                 }
 
@@ -76,51 +92,51 @@ namespace g3
                 for (int j = 0; j < 3; ++j) {
                     int a = tv[j], b = tv[(j + 1) % 3];
                     e[j] = FindEdge(a, b);
-                    DMESH_CHECK_OR_FAIL(e[j] != InvalidID);
-                    DMESH_CHECK_OR_FAIL(edge_has_t(e[j], tID));
-                    DMESH_CHECK_OR_FAIL(e[j] == FindEdgeFromTri(a, b, tID));
+                    CheckOrFailF(e[j] != InvalidID);
+                    CheckOrFailF(edge_has_t(e[j], tID));
+                    CheckOrFailF(e[j] == FindEdgeFromTri(a, b, tID));
                 }
-                DMESH_CHECK_OR_FAIL(e[0] != e[1] && e[0] != e[2] && e[1] != e[2]);
+                CheckOrFailF(e[0] != e[1] && e[0] != e[2] && e[1] != e[2]);
 
                 // tri nbrs must exist and reference this tri, or same edge must be boundary edge
                 Index3i te = GetTriEdges(tID);
                 for (int j = 0; j < 3; ++j) {
                     int eid = te[j];
-                    DMESH_CHECK_OR_FAIL(IsEdge(eid));
+                    CheckOrFailF(IsEdge(eid));
                     int tOther = edge_other_t(eid, tID);
                     if (tOther == InvalidID) {
-                        DMESH_CHECK_OR_FAIL(tri_is_boundary(tID));
+                        CheckOrFailF(tri_is_boundary(tID));
                         continue;
                     }
 
-                    DMESH_CHECK_OR_FAIL( tri_has_neighbour_t(tOther, tID) == true);
+                    CheckOrFailF( tri_has_neighbour_t(tOther, tID) == true);
 
                     // edge must have same two verts as tri for same index
                     int a = tv[j], b = tv[(j + 1) % 3];
                     Index2i ev = GetEdgeV(te[j]);
-                    DMESH_CHECK_OR_FAIL(IndexUtil.same_pair_unordered(a, b, ev[0], ev[1]));
+                    CheckOrFailF(IndexUtil.same_pair_unordered(a, b, ev[0], ev[1]));
 
                     // also check that nbr edge has opposite orientation
                     Index3i othertv = GetTriangle(tOther);
                     int found = IndexUtil.find_tri_ordered_edge(b, a, othertv.array);
-                    DMESH_CHECK_OR_FAIL(found != InvalidID);
+                    CheckOrFailF(found != InvalidID);
                 }
             }
 
 
             // edge verts/tris must exist
             foreach (int eID in EdgeIndices() ) { 
-                DMESH_CHECK_OR_FAIL(IsEdge(eID));
-                DMESH_CHECK_OR_FAIL(edges_refcount.refCount(eID) == 1);
+                CheckOrFailF(IsEdge(eID));
+                CheckOrFailF(edges_refcount.refCount(eID) == 1);
                 Index2i ev = GetEdgeV(eID);
                 Index2i et = GetEdgeT(eID);
-                DMESH_CHECK_OR_FAIL(IsVertex(ev[0]));
-                DMESH_CHECK_OR_FAIL(IsVertex(ev[1]));
-                DMESH_CHECK_OR_FAIL(et[0] != InvalidID);
-                DMESH_CHECK_OR_FAIL(ev[0] < ev[1]);
-                DMESH_CHECK_OR_FAIL(IsTriangle(et[0]));
+                CheckOrFailF(IsVertex(ev[0]));
+                CheckOrFailF(IsVertex(ev[1]));
+                CheckOrFailF(et[0] != InvalidID);
+                CheckOrFailF(ev[0] < ev[1]);
+                CheckOrFailF(IsTriangle(et[0]));
                 if (et[1] != InvalidID) {
-                    DMESH_CHECK_OR_FAIL(IsTriangle(et[1]));
+                    CheckOrFailF(IsTriangle(et[1]));
                 }
             }
 
@@ -128,64 +144,63 @@ namespace g3
             bool is_compact = vertices_refcount.is_dense;
             if (is_compact) {
                 for (int vid = 0; vid < vertices.Length / 3; ++vid) {
-                    DMESH_CHECK_OR_FAIL(vertices_refcount.isValid(vid));
+                    CheckOrFailF(vertices_refcount.isValid(vid));
                 }
             }
 
             // vertex edges must exist and reference this vert
             foreach( int vID in VertexIndices()) { 
-                DMESH_CHECK_OR_FAIL(IsVertex(vID));
+                CheckOrFailF(IsVertex(vID));
 
                 Vector3d v = GetVertex(vID);
-                DMESH_CHECK_OR_FAIL(double.IsNaN(v.LengthSquared) == false);
-                DMESH_CHECK_OR_FAIL(double.IsInfinity(v.LengthSquared) == false);
+                CheckOrFailF(double.IsNaN(v.LengthSquared) == false);
+                CheckOrFailF(double.IsInfinity(v.LengthSquared) == false);
 
                 List<int> l = vertex_edges[vID];
                 foreach(int edgeid in l) { 
-                    DMESH_CHECK_OR_FAIL(IsEdge(edgeid));
-                    DMESH_CHECK_OR_FAIL(edge_has_v(edgeid, vID));
+                    CheckOrFailF(IsEdge(edgeid));
+                    CheckOrFailF(edge_has_v(edgeid, vID));
 
                     int otherV = edge_other_v(edgeid, vID);
                     int e2 = find_edge(vID, otherV);
-                    DMESH_CHECK_OR_FAIL(e2 != InvalidID);
-                    DMESH_CHECK_OR_FAIL(e2 == edgeid);
+                    CheckOrFailF(e2 != InvalidID);
+                    CheckOrFailF(e2 == edgeid);
                     e2 = find_edge(otherV, vID);
-                    DMESH_CHECK_OR_FAIL(e2 != InvalidID);
-                    DMESH_CHECK_OR_FAIL(e2 == edgeid);
+                    CheckOrFailF(e2 != InvalidID);
+                    CheckOrFailF(e2 == edgeid);
                 }
 
 				List<int> vTris = new List<int>(), vTris2 = new List<int>();
                 GetVtxTriangles(vID, vTris, false);
 				GetVtxTriangles(vID, vTris2, true);
-				DMESH_CHECK_OR_FAIL(vTris.Count == vTris2.Count);
+				CheckOrFailF(vTris.Count == vTris2.Count);
 				//System.Console.WriteLine(string.Format("{0} {1} {2}", vID, vTris.Count, GetVtxEdges(vID).Count));
                 if ( bAllowNonManifoldVertices )
-    				DMESH_CHECK_OR_FAIL(vTris.Count <= GetVtxEdges(vID).Count);
+    				CheckOrFailF(vTris.Count <= GetVtxEdges(vID).Count);
                 else
-    				DMESH_CHECK_OR_FAIL(vTris.Count == GetVtxEdges(vID).Count || vTris.Count == GetVtxEdges(vID).Count-1);
-                DMESH_CHECK_OR_FAIL(vertices_refcount.refCount(vID) == vTris.Count + 1);
-                DMESH_CHECK_OR_FAIL(triToVtxRefs[vID] == vTris.Count);
+    				CheckOrFailF(vTris.Count == GetVtxEdges(vID).Count || vTris.Count == GetVtxEdges(vID).Count-1);
+                CheckOrFailF(vertices_refcount.refCount(vID) == vTris.Count + 1);
+                CheckOrFailF(triToVtxRefs[vID] == vTris.Count);
                 foreach( int tID in vTris) {
-                    DMESH_CHECK_OR_FAIL(tri_has_v(tID, vID));
+                    CheckOrFailF(tri_has_v(tID, vID));
                 }
 
 				// check that edges around vert only references tris above, and reference all of them!
 				List<int> vRemoveTris = new List<int>(vTris);
 				foreach ( int edgeid in l ) {
 					Index2i edget = GetEdgeT(edgeid);
-					DMESH_CHECK_OR_FAIL( vTris.Contains(edget[0]) );
+					CheckOrFailF( vTris.Contains(edget[0]) );
 					if ( edget[1] != InvalidID )
-						DMESH_CHECK_OR_FAIL( vTris.Contains(edget[1]) );
+						CheckOrFailF( vTris.Contains(edget[1]) );
 					vRemoveTris.Remove(edget[0]);
 					if ( edget[1] != InvalidID )
 						vRemoveTris.Remove(edget[1]);
 				}
-				DMESH_CHECK_OR_FAIL(vRemoveTris.Count == 0);
+				CheckOrFailF(vRemoveTris.Count == 0);
             }
-            return true;
+
+            return is_ok;
         }
-        void check_or_fail(bool bCondition) {
-            Util.gDevAssert(bCondition);
-        }
+
     }
 }
