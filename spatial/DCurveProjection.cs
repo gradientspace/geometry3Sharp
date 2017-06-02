3a952656a228e94b41825fcccfc74afec0c6e308
rms <rms@rms80.com>
2017-06-02 14:56:03 -0400

    handle open curves

--- spatial/DCurveProjection.cs
+++ spatial/DCurveProjection.cs
@@ -20,7 +20,7 @@ namespace g3
             Vector3d vNearest = Vector3d.Zero;
             double fNearestSqr = double.MaxValue;
 
-            int N = Curve.VertexCount;
+            int N = (Curve.Closed) ? Curve.VertexCount : Curve.VertexCount - 1;
             for ( int i = 0; i < N; ++i ) {
                 Segment3d seg = new Segment3d(Curve[i], Curve[(i + 1) % N]);
                 Vector3d pt = seg.NearestPoint(vPoint);
