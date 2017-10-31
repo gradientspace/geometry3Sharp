45b95cd6591afdd2e030566c6d44b5afc0955749
rms <rms@rms80.com>
2017-10-31 11:46:26 -0400

    added color lighten/darken funcs

--- color/ColorMixer.cs
+++ color/ColorMixer.cs
@@ -4,9 +4,22 @@ using System.Collections.Generic;
 
 namespace g3
 {
-    public class ColorMixer
+    public static class ColorMixer
     {
 
+        public static Colorf Lighten(Colorf baseColor, float fValueMult = 1.25f)
+        {
+            ColorHSV baseHSV = new ColorHSV(baseColor);
+            baseHSV.v = MathUtil.Clamp(baseHSV.v * fValueMult, 0.0f, 1.0f);
+            return baseHSV.ConvertToRGB();
+        }
+
+        public static Colorf Darken(Colorf baseColor, float fValueMult = 0.75f)
+        {
+            ColorHSV baseHSV = new ColorHSV(baseColor);
+            baseHSV.v *= fValueMult;
+            return baseHSV.ConvertToRGB();
+        }
 
 
         public static Colorf CopyHue(Colorf BaseColor, Colorf TakeHue, float fBlendAlpha)
