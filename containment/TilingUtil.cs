f2cd7d4ad7c661f18805015d7b0541ecbbdff79b
Ryan Schmidt <rms@rms80.com>
2017-10-30 21:49:06 -0400

    add Hexagon2, w/ some basic hex-math functions. add 2d circle-packing tiling

--- containment/TilingUtil.cs
+++ containment/TilingUtil.cs
@@ -25,7 +25,7 @@ namespace g3
 
 			int ny = Math.Max(1, (int)(bounds.Height / h));
 			double spaceh = (ny - 1) * spacing;
-			while (ny > 1 && bounds.Height - (w * ny + spaceh) < 0)
+			while (ny > 1 && bounds.Height - (h * ny + spaceh) < 0)
 				ny--;
 
 			List<Vector2d> translations = new List<Vector2d>();
@@ -40,6 +40,81 @@ namespace g3
 			return translations;
 		}
 
+
+
+
+
+		/// <summary>
+		/// hex-grid tiling of circles inside bounds, with spacing between elements
+		/// Returns list of translations to element.
+		/// Always allows at least one row and column, even if element overflows bounds in that dimension.
+		/// </summary>
+		public static List<Vector2d> BoundedCircleTiling2(AxisAlignedBox2d element, AxisAlignedBox2d bounds,
+													   	  double spacing)
+		{
+			Vector2d oshift = -element.Min;
+			double w = element.Width; double h = element.Height;
+			if (MathUtil.EpsilonEqual(w, h) == false)
+				throw new Exception("BoundedHexTiling2: input box is not square");
+
+			// note: this is a circle tiling, not a hex tiling, so even though we are
+			// starting in top-left with a "tip" hex, we don't have to offset down so that tip is inside
+			// the box (circle cuts off)
+
+			double r = w / 2;
+			Hexagon2d hex = new Hexagon2d(element.Center, r, Hexagon2d.TopModes.Tip);
+			hex.InnerRadius = r;
+
+			double stepx = hex.HorzSpacing;
+			double stepy = hex.VertSpacing;
+
+			double spacingy = spacing;
+			double spacingx = spacing;
+
+			// half-rows on top and bottom add up to full row-height
+			int ny = Math.Max(1, (int)(bounds.Height / stepy));
+			// reduce count so that we fit w/ spacing
+			double spaceh = (ny - 1) * spacingy;
+			while (ny > 1 && bounds.Height - (stepy * ny + spaceh) < 0)
+				ny--;
+
+			// even rows start at x=0
+			int nx_even = Math.Max(1, (int)(bounds.Width / stepx));
+			// reduce count if we spill over w/ spacing
+			double spacew = (nx_even - 1) * spacingx;
+			while (nx_even > 1 && bounds.Width - (stepx * nx_even + spacew) < 0)
+				nx_even--;
+
+			// odd rows have an extra half-step added to left side,
+			// so we may need to reduce count
+			int nx_odd = nx_even;
+			spacew = (nx_odd - 1) * spacingx;
+			if (ny > 0 && (stepx * nx_odd + spacew + stepx * 0.5) > bounds.Width) {
+				nx_odd--;
+				spacew = (nx_odd - 1) * spacingx;
+			}
+
+
+			List<Vector2d> translations = new List<Vector2d>();
+			for (int yi = 0; yi < ny; ++yi) {
+				double dy = yi*stepy + yi*spacingy;
+
+				// x shift and count are different on odd rows
+				double shiftx = stepx * 0.5;
+				int nx = nx_odd;
+				if (yi % 2 == 0) {
+					shiftx = 0;
+					nx = nx_even;
+				}
+
+				for (int xi = 0; xi < nx; ++xi) {
+					double dx = shiftx + xi * stepx + xi*spacingx;
+					translations.Add(new Vector2d(dx, dy) + oshift + bounds.Min);
+				}
+			}
+
+			return translations;
+		}
 
 	}
 }
---
+++ curve/Hexagon2.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace g3
+{
+    public class Hexagon2d
+    {
+		public enum TopModes
+		{
+			Flat = 0,
+			Tip = 1
+		}
+
+		public Vector2d Center;
+		public double Radius;   // distance from center to corners
+		public TopModes TopMode;
+
+		public Hexagon2d(Vector2d center, double radius, TopModes mode = TopModes.Flat)
+		{
+			Center = center;
+			Radius = radius;
+			TopMode = mode;
+		}
+
+
+		public bool IsClosed {
+			get { return true; }
+		}
+
+
+		public Hexagon2d Clone() {
+			return new Hexagon2d(this.Center, this.Radius, this.TopMode);
+        }
+
+
+
+		public double InnerRadius {
+			get { return MathUtil.SqrtThree * Radius / 2.0; }
+			set { Radius = (2.0 * value) / MathUtil.SqrtThree; }
+		}
+
+
+		public Vector2d Corner(int i) {
+			double angle_deg = 60.0 * (double)i;
+			if (TopMode == TopModes.Tip)
+				angle_deg += 30;
+			double angle_rad = angle_deg * MathUtil.Deg2Rad;
+			return new Vector2d(Center.x + Radius * Math.Cos(angle_rad),
+								Center.y + Radius * Math.Sin(angle_rad));
+		}
+
+
+		public double Width {
+			get {
+				return (TopMode == TopModes.Flat) ? (Radius * 2) : ((MathUtil.SqrtThree / 2.0) * Height);
+			}
+		}
+		public double Height {
+			get {
+				return (TopMode == TopModes.Flat) ? ((MathUtil.SqrtThree / 2.0) * Width) : (Radius * 2);
+			}
+		}
+
+
+		public double VertSpacing {
+			get {
+				return (TopMode == TopModes.Flat) ? Height : (Height * 3.0 / 4.0);
+			}
+		}
+
+		public double HorzSpacing {
+			get {
+				return (TopMode == TopModes.Flat) ? (Width * 3.0 / 4.0) : Width;
+			}
+		}
+
+
+
+		//public bool HasArcLength { get {return true;} }
+
+		//public double ArcLength {
+		//	get {
+		//		return MathUtil.TwoPI * Radius;
+		//	}
+		//}
+
+
+		//public Vector2d SampleArcLength(double a) {
+		//	double t = a / ArcLength;
+		//	double theta = (IsReversed) ? -t*MathUtil.TwoPI : t*MathUtil.TwoPI;
+		//	double c = Math.Cos(theta), s = Math.Sin(theta);
+		//	return new Vector2d(Center.x + Radius*c, Center.y + Radius*s);
+		//}
+
+
+  //      public bool Contains (Vector2d p ) {
+  //          double d = Center.DistanceSquared(p);
+  //          return d <= Radius * Radius;
+  //      }
+
+
+  //      public double Circumference {
+		//	get { return MathUtil.TwoPI * Radius; }
+  //          set { Radius = value / MathUtil.TwoPI; }
+		//}
+  //      public double Diameter {
+		//	get { return 2 * Radius; }
+  //          set { Radius = value / 2; }
+		//}
+        //public double Area {
+        //    get { return Math.PI * Radius * Radius; }
+        //    set { Radius = Math.Sqrt(value / Math.PI); }
+        //}
+
+		public AxisAlignedBox2d Bounds {
+			get { return new AxisAlignedBox2d(Center, Width/2, Height/2); }
+		}
+
+        //public double SignedDistance(Vector2d pt)
+        //{
+        //    double d = Center.Distance(pt);
+        //    return d - Radius;
+        //}
+        //public double Distance(Vector2d pt)
+        //{
+        //    double d = Center.Distance(pt);
+        //    return Math.Abs(d - Radius);
+        //}
+
+    }
+}
