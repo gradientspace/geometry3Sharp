2bf8c708a289f247c7b897f9c8966a28b4f89dc5
rms <rms@rms80.com>
2017-11-09 10:07:02 -0500

    restore this file for older csproj format

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿#if !G3_USING_UNITY
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("geometry3Sharp")]
+[assembly: AssemblyDescription("C# library for 3D geometric computation, mesh algorithms, etc.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ryan Schmidt/gradientspace")]
+[assembly: AssemblyProduct("geometry3Sharp")]
+[assembly: AssemblyCopyright("Copyright © Ryan Schmidt/gradientspace  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0c518dda-28fe-44ca-9ab0-f9773974f13a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+#endif
