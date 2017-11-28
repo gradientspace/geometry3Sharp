dff1123214b4bcab37c925ac2a554cde57f67ab8
rms <rms@rms80.com>
2017-11-27 23:05:02 -0500

    improved scheme for mapping vertices to sphere

--- mesh_generators/SphereGenerators.cs
+++ mesh_generators/SphereGenerators.cs
@@ -14,11 +14,30 @@ namespace g3
     {
         public double Radius = 1.0;
 
+        public enum NormalizationTypes
+        {
+            NormalizedVector,
+            CubeMapping             // produces more even distribution of quads
+                                    // see http://catlikecoding.com/unity/tutorials/cube-sphere/
+                                    // or http://mathproofs.blogspot.ca/2005/07/mapping-cube-to-sphere.html
+        }
+        NormalizationTypes NormalizeType = NormalizationTypes.CubeMapping;
+
         public override MeshGenerator Generate()
         {
             base.Generate();
             for ( int i = 0; i < vertices.Count; ++i ) {
                 Vector3d v = vertices[i] - Box.Center;
+                if (NormalizeType == NormalizationTypes.CubeMapping) {
+                    double x = v.Dot(Box.AxisX) / Box.Extent.x;
+                    double y = v.Dot(Box.AxisY) / Box.Extent.y;
+                    double z = v.Dot(Box.AxisZ) / Box.Extent.z;
+                    double x2 = x * x, y2 = y * y, z2 = z * z;
+                    double sx = x * Math.Sqrt(1.0 - y2*0.5 - z2*0.5 + y2*z2/3.0);
+                    double sy = y * Math.Sqrt(1.0 - x2*0.5 - z2*0.5 + x2*z2/3.0);
+                    double sz = z * Math.Sqrt(1.0 - x2*0.5 - y2*0.5 + x2*y2/3.0);
+                    v = sx*Box.AxisX + sy*Box.AxisY + sz*Box.AxisZ;
+                }
                 v.Normalize();
                 vertices[i] = Box.Center + Radius*v;
                 normals[i] = (Vector3f)v;
