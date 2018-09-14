73aa879ae3bc7244a9b306c48e27db38260d7711
Ryan Schmidt <rms@rms80.com>
2018-09-14 13:41:19 -0400

    implementation of fast point winding number

--- math/FastWindingMath.cs
+++ math/FastWindingMath.cs
@@ -166,8 +166,97 @@ namespace g3
     /// </summary>
     public static class FastPointWinding
     {
+        /// <summary>
+        /// precompute constant coefficients of point winding number approximation
+        /// pointAreas must be provided, and pointSet must have vertex normals!
+        /// p: 'center' of expansion for points (area-weighted point avg)
+        /// r: max distance from p to points
+        /// order1: first-order vector coeff
+        /// order2: second-order matrix coeff
+        /// </summary>
+        public static void ComputeCoeffs(
+            IPointSet pointSet, IEnumerable<int> points, double[] pointAreas,
+            ref Vector3d p, ref double r,
+            ref Vector3d order1, ref Matrix3d order2 )
+        {
+            if (pointSet.HasVertexNormals == false)
+                throw new Exception("FastPointWinding.ComputeCoeffs: point set does not have normals!");
 
-        public static double ExactEval(ref Vector3d x, ref Vector3d xn, ref double xA, ref Vector3d q)
+            p = Vector3d.Zero;
+            order1 = Vector3d.Zero;
+            order2 = Matrix3d.Zero;
+            r = 0;
+
+            // compute area-weighted centroid of points, we use this as the expansion point
+            double sum_area = 0;
+            foreach (int vid in points) {
+                sum_area += pointAreas[vid];
+                p += pointAreas[vid] * pointSet.GetVertex(vid);
+            }
+            p /= sum_area;
+
+            // compute first and second-order coefficients of FWN taylor expansion, as well as
+            // 'radius' value r, which is max dist from any tri vertex to p  
+            foreach (int vid in points) {
+                Vector3d p_i = pointSet.GetVertex(vid);
+                Vector3d n_i = pointSet.GetVertexNormal(vid);
+                double a_i = pointAreas[vid];
+
+                order1 += a_i * n_i;
+
+                Vector3d dcp = p_i - p;
+                order2 += a_i * new Matrix3d(ref dcp, ref n_i);
+
+                // this is just for return value...
+                r = Math.Max(r, p_i.Distance(p));
+            }
+        }
+
+
+        /// <summary>
+        /// Evaluate first-order FWN approximation at point q, relative to center c
+        /// </summary>
+        public static double EvaluateOrder1Approx(ref Vector3d center, ref Vector3d order1Coeff, ref Vector3d q)
+        {
+            Vector3d dpq = (center - q);
+            double len = dpq.Length;
+
+            return (1.0 / MathUtil.FourPI) * order1Coeff.Dot(dpq / (len * len * len));
+        }
+
+
+
+        /// <summary>
+        /// Evaluate second-order FWN approximation at point q, relative to center c
+        /// </summary>
+        public static double EvaluateOrder2Approx(ref Vector3d center, ref Vector3d order1Coeff, ref Matrix3d order2Coeff, ref Vector3d q)
+        {
+            Vector3d dpq = (center - q);
+            double len = dpq.Length;
+            double len3 = len * len * len;
+            double fourPi_len3 = 1.0 / (MathUtil.FourPI * len3);
+
+            double order1 = fourPi_len3 * order1Coeff.Dot(ref dpq);
+
+            // second-order hessian \grad^2(G)
+            double c = -3.0 / (MathUtil.FourPI * len3 * len * len);
+
+            // expanded-out version below avoids extra constructors
+            //Matrix3d xqxq = new Matrix3d(ref dpq, ref dpq);
+            //Matrix3d hessian = new Matrix3d(fourPi_len3, fourPi_len3, fourPi_len3) - c * xqxq;
+            Matrix3d hessian = new Matrix3d(
+                fourPi_len3 + c * dpq.x * dpq.x, c * dpq.x * dpq.y, c * dpq.x * dpq.z,
+                c * dpq.y * dpq.x, fourPi_len3 + c * dpq.y * dpq.y, c * dpq.y * dpq.z,
+                c * dpq.z * dpq.x, c * dpq.z * dpq.y, fourPi_len3 + c * dpq.z * dpq.z);
+
+            double order2 = order2Coeff.InnerProduct(ref hessian);
+
+            return order1 + order2;
+        }
+
+
+
+        public static double ExactEval(ref Vector3d x, ref Vector3d xn, double xA, ref Vector3d q)
         {
             Vector3d dv = (x - q);
             double len = dv.Length;
@@ -176,7 +265,7 @@ namespace g3
 
         // point-winding-number first-order approximation. 
         // x is dipole point, p is 'center' of cluster of dipoles, q is evaluation point
-        public static double Order1Approx(ref Vector3d x, ref Vector3d p, ref Vector3d xn, ref double xA, ref Vector3d q)
+        public static double Order1Approx(ref Vector3d x, ref Vector3d p, ref Vector3d xn, double xA, ref Vector3d q)
         {
             Vector3d dpq = (p - q);
             double len = dpq.Length;
@@ -188,7 +277,7 @@ namespace g3
 
         // point-winding-number second-order approximation
         // x is dipole point, p is 'center' of cluster of dipoles, q is evaluation point
-        public static double Order2Approx(ref Vector3d x, ref Vector3d p, ref Vector3d xn, ref double xA, ref Vector3d q)
+        public static double Order2Approx(ref Vector3d x, ref Vector3d p, ref Vector3d xn, double xA, ref Vector3d q)
         {
             Vector3d dpq = (p - q);
             Vector3d dxp = (x - p);
