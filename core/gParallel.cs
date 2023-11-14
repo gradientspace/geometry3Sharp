e1773e1175e1425fc661bb66b8b4b0ae8f7d1d49
Paul <runette@gmail.com>
2023-11-14 11:29:53 +0000

    package tested

--- core/gParallel.cs
+++ core/gParallel.cs
@@ -20,7 +20,7 @@ namespace g3
         }
         public static void ForEach<T>( IEnumerable<T> source, Action<T> body )
         {
-NET_2_0 || NET_2_0_SUBSET
+#if NET_2_0 || NET_2_0_SUBSET
             for_each<T>(source, body);
 #else
             Parallel.ForEach<T>(source, body);
@@ -274,7 +274,7 @@ NET_2_0 || NET_2_0_SUBSET
 
 
 
-NET_2_0 || NET_2_0_SUBSET
+#if NET_2_0 || NET_2_0_SUBSET
 
     /*
      * .NET 3.5 (default in Unity) does not have SpinLock object, which we
